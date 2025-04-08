7ebda3e43c030711291c254389452309b6185001
Júnior Nascimento <krymancer@protonmail.com>
2025-04-08 01:16:25 -0300

    fix: fixing breadcrumbs

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,8 +1,25 @@
 import { db } from "~/server/db";
 import { files as filesSchema, folders, folders as foldersSchema } from "~/server/db/schema";
 import DriveContents from "../../drive-contents";
-import { z } from "zod";
 import { eq } from "drizzle-orm";
+
+async function getAllParents(folderId: number) {
+  const parents = [];
+  let currentId: number | null = folderId;
+
+  while (currentId !== null) {
+    const [folder] = await db.select().from(foldersSchema).where(eq(foldersSchema.id, currentId));
+
+    if (!folder) {
+      throw new Error(`Folder with ID ${currentId} not found`);
+    }
+
+    parents.unshift(folder);
+    currentId = folder.parent;
+  }
+
+  return parents;
+}
 
 export default async function GoogleDriveClone(props: { params: Promise<{ folderId: string }> }) {
   const params = await props.params;
@@ -12,9 +29,13 @@ export default async function GoogleDriveClone(props: { params: Promise<{ folder
     return <div>Invalid folder ID</div>;
   }
 
-  const files = await db.select().from(filesSchema).where(eq(filesSchema.parent, parsedFolderId));
-  const folders = await db.select().from(foldersSchema).where(eq(foldersSchema.parent, parsedFolderId));
+  const foldersPromise = db.select().from(foldersSchema).where(eq(foldersSchema.parent, parsedFolderId));
+  const filesPromise = db.select().from(filesSchema).where(eq(filesSchema.parent, parsedFolderId));
 
-  return <DriveContents files={files} folders={folders} />;
+  const parentsPromise = getAllParents(parsedFolderId);
+
+  const [folders, files, parents] = await Promise.all([foldersPromise, filesPromise, parentsPromise]);
+
+  return <DriveContents files={files} folders={folders} parents={parents} />;
 }
 
