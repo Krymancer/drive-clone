f1bce1cea0b4d6e71c8699c1320e2eb2a78b3532
Júnior Nascimento <krymancer@protonmail.com>
2025-04-08 00:32:24 -0300

    feat: render from database

--- src/env.js
+++ src/env.js
@@ -7,7 +7,6 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -32,7 +31,6 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     SINGLESTORE_DATABASE_HOST: process.env.SINGLESTORE_DATABASE_HOST,
     SINGLESTORE_DATABASE_PORT: process.env.SINGLESTORE_DATABASE_PORT,
