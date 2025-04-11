537ecd1126781574719b5aaa6152fc2df87c9f2b
Júnior Nascimento <krymancer@protonmail.com>
2025-04-11 09:55:51 -0300

    feat: some improviments

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,6 +1,6 @@
 import "server-only";
 
-import { eq } from "drizzle-orm";
+import { eq, and, isNull } from "drizzle-orm";
 import { db } from ".";
 import { fileSchema, folderSchema, type FileInsertType } from "./schema";
 
@@ -23,13 +23,25 @@ export const QUERIES = {
     return parents;
   },
   getFolders: async function (parentId: number) {
-    return db.select().from(folderSchema).where(eq(folderSchema.parent, parentId));
+    return db.select().from(folderSchema).where(eq(folderSchema.parent, parentId)).orderBy(folderSchema.createdAt);
   },
   getFiles: async function (parentId: number) {
-    return db.select().from(fileSchema).where(eq(fileSchema.parent, parentId));
+    return db.select().from(fileSchema).where(eq(fileSchema.parent, parentId)).orderBy(fileSchema.createdAt);
   },
   getFolderById: async function (folderId: number) {
     const [folder] = await db.select().from(folderSchema).where(eq(folderSchema.id, folderId));
+    return folder;
+  },
+  getRootFolderForUser: async function (userId: string) {
+    const [folder] = await db
+      .select()
+      .from(folderSchema)
+      .where(
+        and(
+          eq(folderSchema.ownerId, userId),
+          isNull(folderSchema.parent)
+        )
+      );
     return folder;
   }
 };
@@ -37,5 +49,37 @@ export const QUERIES = {
 export const MUTATIONS = {
   createFile: async function (input: { file: FileInsertType, userId: string }) {
     return await db.insert(fileSchema).values(input.file);
-  }
+  },
+  onboardUser: async function (userId: string) {
+    const [rootFolder] = await db
+      .insert(folderSchema)
+      .values({
+        name: "Root",
+        parent: null,
+        ownerId: userId,
+      })
+      .$returningId();
+
+    const rootFolderId = rootFolder!.id;
+
+    await db.insert(folderSchema).values([
+      {
+        name: "Trash",
+        parent: rootFolderId,
+        ownerId: userId,
+      },
+      {
+        name: "Shared",
+        parent: rootFolderId,
+        ownerId: userId,
+      },
+      {
+        name: "Documents",
+        parent: rootFolderId,
+        ownerId: userId,
+      },
+    ]);
+
+    return rootFolderId;
+  },
 }
